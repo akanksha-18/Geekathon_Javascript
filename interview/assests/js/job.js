87bc842a88c93583490a23d12f4a6678c8fe68e7
Akanksha Dubey <akankshadubey2558@gmail.com>
2024-04-01 14:55:38 +0530

    features implemented

---
+++ interview/assests/js/job.js
@@ -0,0 +1,63 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+const API_KEY = "AIzaSyC1oRcJ7eAQaAO4kBqTJMt2w-ekoOCfj-I"; // Replace with your actual API key
+            const genAI = new GoogleGenerativeAI(API_KEY);
+        
+            document.getElementById("jobRecommendationBtn").addEventListener("click", getJobRecommendations);
+        
+            async function getJobRecommendations() {
+                const skills = getSkills();
+                const jobRecommendations = await generateJobRecommendations(skills);
+                displayJobRecommendations(jobRecommendations);
+            }
+            async function generateJobRecommendations(skills) {
+                showSpinner();
+                const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+                const results = await Promise.all(skills.map(skill => model.generateContent(skill + " jobs and companies")));
+                const jobRecommendations = results.flatMap(result => {
+                    const response = result.response;
+                    return response.text().split("\n\n");
+                });
+                hideSpinner();
+                return jobRecommendations;
+               
+            }
+            function convertMarkdownToHtml(markdownText) {
+    const md = window.markdownit();
+    return md.render(markdownText);
+}
+        
+            function displayJobRecommendations(jobRecommendations) {
+                const recommendationsContainer = document.querySelector(".job-recommendations");
+                // recommendationsContainer.innerHTML = jobRecommendations.join("<br>");
+                recommendationsContainer.innerHTML = convertMarkdownToHtml(jobRecommendations.join("\n\n"));
+            }
+        
+            function getSkills() {
+                const skillsInputs = document.querySelectorAll(".skill");
+                const skills = [];
+                skillsInputs.forEach(input => {
+                    const skill = input.value.trim();
+                    if (skill) {
+                        skills.push(skill);
+                    }
+                });
+                return skills;
+            }
+
+            const spinnerContainer = document.querySelector('.spinner-container');
+
+    // async function getJobRecommendations() {
+    //     showSpinner();
+    //     const skills = getSkills();
+    //     const jobRecommendations = await generateJobRecommendations(skills);
+    //     displayJobRecommendations(jobRecommendations);
+    //     hideSpinner();
+    // }
+
+    function showSpinner() {
+        spinnerContainer.style.display = 'block';
+    }
+
+    function hideSpinner() {
+        spinnerContainer.style.display = 'none';
+    }
